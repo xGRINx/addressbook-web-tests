e5de17283d738294b6a94a3793bfc013a85255a4
xGRINx <41790801+xGRINx@users.noreply.github.com>
2018-10-09 17:21:03 +0300

    Изменения по 2.8, общий базовый класс для классов-помощников 2

--- src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,14 +3,14 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase {
     private WebDriver driver;
 
     public NavigationHelper(WebDriver driver) {
-        this.driver = driver;
+        super (driver);
     }
 
     public void gotoGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 }
