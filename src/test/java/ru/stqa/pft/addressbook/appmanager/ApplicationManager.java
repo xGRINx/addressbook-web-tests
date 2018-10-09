41ea65e72ee602cb2c2adc0571e4b4345452bf4c
xGRINx <41790801+xGRINx@users.noreply.github.com>
2018-10-09 16:49:38 +0300

    Изменение по 2.7, построение двухуровневой структуры тестов

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,11 +3,13 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+    private SessionHelper sessionHelper;
     public StringBuffer verificationErrors = new StringBuffer();
     public WebDriver driver;
 
@@ -15,56 +17,23 @@ public class ApplicationManager {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://localhost:8080/addressbook/group.php");
-        login("admin", "secret");
+        groupHelper = new GroupHelper(driver);
+        navigationHelper = new NavigationHelper(driver);
+        sessionHelper = new SessionHelper(driver);
+        sessionHelper.login("admin", "secret");
     }
 
-    public void login(String username, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.id("LoginForm")).submit();
-    }
 
-    public void returnToGroupPage() {
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    public void submitGroupCreation() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    public void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    public void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    public void gotoGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
 
     public void stop() {
         driver.quit();
     }
 
-    public void deleteSelectedGroups() {
-        driver.findElement(By.name("delete")).click();
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
     }
 
-    public void selectGroup() {
-        driver.findElement(By.name("selected[]")).click();
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
     }
 }
---
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SessionHelper {
+    private WebDriver driver;
+
+    public SessionHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String username, String password) {
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(username);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(By.id("LoginForm")).submit();
+    }
+}
